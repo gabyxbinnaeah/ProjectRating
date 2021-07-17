77a06ded2324b57641cd18950d8689e63b952068
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-17 17:17:22 +0300

    adds profile update form

--- ratingapp/forms.py
+++ ratingapp/forms.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Profile
 
 
 class CreateUserForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
+
+class UpdateProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+    
+
