bcdff211ccb06898d2202a0727ab5cf6a78d3922
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-17 13:50:34 +0300

    adds logout button and profile user name interpolation to navabar

--- ratingapp/urls.py
+++ ratingapp/urls.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
-from . import views 
+from . import views  
 
 urlpatterns=[
     url('',views.index, name='index'),
+    url('register/', views.registerPage, name="register"),
+	url('login/', views.loginPage, name="login"), 
+    url('logout/',views.logoutUser,name="logout") ,
 
 ]
