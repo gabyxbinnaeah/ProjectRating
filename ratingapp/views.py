9e38b3cc5687a6f99bfcc6d1c285e5423c23f6bc
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-17 13:09:50 +0300

    creates the register view function

--- ratingapp/views.py
+++ ratingapp/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('gallery')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('login')
+			
+
+		context = {'form':form}
+		return render(request, 'accounts/register.html', context)
 
 # Create your views here.
 def index(request):
