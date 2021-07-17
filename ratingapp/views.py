c82201c0ef42b06e3c3815e39497e3543929f451
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-17 13:16:08 +0300

    creates the login  method that lets in only the authenticated users and redirect to index page

--- ratingapp/views.py
+++ ratingapp/views.py
@@ -23,6 +23,26 @@ def registerPage(request):
 
 		context = {'form':form}
 		return render(request, 'accounts/register.html', context)
+        
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('gallery')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('gallery')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'accounts/login.html', context)
+
 
 # Create your views here.
 def index(request):
