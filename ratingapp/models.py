2a36ba8e581c80e9244b9c58edce52737746daf8
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-17 12:53:57 +0300

    creates form for adding users from django models

--- ratingapp/models.py
+++ ratingapp/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db import models 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
 
-# Create your models here.
+
+class CreateUserForm(UserCreationForm):
+	class Meta:
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
