baef9ba5e406fafecf375da15161e1bbd2e7dc87
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-19 19:57:52 +0300

    adds model classes to admin dashboard

--- ratingapp/admin.py
+++ ratingapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile,Project,rating_content_object,rating_design_object,rating_usability_object
 
 
+
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(rating_content_object)
+admin.site.register(rating_design_object)
+admin.site.register(rating_usability_object)
